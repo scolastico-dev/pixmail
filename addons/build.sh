727b17cabd5b28434b1a3edc8d59a8a762dad95f
Dennis Heinrich <hey@dennis-heinri.ch>
2025-09-05 01:47:11 +0200

    fix: added chrome extension to build artifacts

--- addons/build.sh
+++ addons/build.sh
@@ -9,5 +9,6 @@ mkdir -p dist
 
 echo "Building Thunderbird add-on..."
 cd thunderbird && zip -r ../dist/thunderbird.xpi ./* && cd ..
+cd chrome-extension && zip -r ../dist/chrome-extension.zip ./* && cd ..
 
 echo "All add-ons built successfully."
