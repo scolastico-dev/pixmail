ed7b79d2311cbdd1445ef5c527234a6a613ee4cd
Joschua Becker <jb@scolasti.co>
2025-09-06 14:20:17 +0200

    chore: renamed chrome-extension -> chrome; split of nightly ci

--- addons/build.sh
+++ addons/build.sh
@@ -9,6 +9,6 @@ mkdir -p dist
 
 echo "Building Thunderbird add-on..."
 cd thunderbird && zip -r ../dist/thunderbird.xpi ./* && cd ..
-cd chrome-extension && zip -r ../dist/chrome-extension.zip ./* && cd ..
+cd chrome && zip -r ../dist/chrome.zip ./* && cd ..
 
 echo "All add-ons built successfully."
